7dc298312d1492c4c4f920b85c540e532dffbccb
dkredd <deepthi.kovvuri@gmail.com>
2023-01-25 09:06:52 -0800

    remove flask 8 and whitespace

--- test_calculator.py
+++ test_calculator.py
@@ -1,13 +1,10 @@
 import calculator
 
+
 class TestCalculator:
 
     def test_add(self):
-        assert 4 == calculator.add(2,2)
-
+        assert 4 == calculator.add(2, 2)
 
     def test_sub(self):
-        assert 2 == calculator.sub(4,2)
-
-
-
+        assert 2 == calculator.sub(4, 2)
